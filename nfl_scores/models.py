dd7644270453ced4fcf5c74e4ffad2c086274ff6
Peter Bertel <pbertel@Peters-MacBook-Pro.local>
2016-11-20 10:27:52 -0600

    Added week_view template and route. Started templating the view of games based on mockup design

--- nfl_scores/models.py
+++ nfl_scores/models.py
@@ -9,34 +9,34 @@ class Team(models.Model):
 	losses = models.IntegerField(default=0)
 
 	def __str__(self):
-		return 'The %s have %s wins and %s losses' (self.name, self.wins, self.losses)
+		return 'The %s have %s wins and %s losses' % (self.name, self.wins, self.losses)
 
-class Week(models.Model):
-	number = models.IntegerField(default=1)
+# class Week(models.Model):
+# 	number = models.IntegerField(default=1)
 
-	def __str__(self):
-		return 'Week %s' (self.number)
+# 	def __str__(self):
+# 		return 'Week %s' % (self.number)
 
 class Game(models.Model):
 	# home and away team references are problematic
-	# home_team = models.ForeignKey(Team, on_delete=models.CASCADE)
-	# away_team = models.ForeignKey(Team, on_delete=models.CASCADE)
-	week = models.ForeignKey(Week, on_delete=models.CASCADE)
+	home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="home_team", null=True)
+	away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="away_team", null=True)
+	week = models.IntegerField(default=1)
 	home_score = models.IntegerField(default=0)
 	away_score = models.IntegerField(default=0)
 	# maybe can do home_score = away_score = models.IntegerField(default=0)
-	tied_game = models.BooleanField(default=False)
+	# tied_game = models.BooleanField(default=False)
 
 	# if home_score > away_score:
-	# 	winner = models.ForeignKey(home_team, on_delete=models.CASCADE)
-	# 	loser = models.ForeignKey(away_team, on_delete=models.CASCADE)
+	# 	winner = models.ForeignKey(home_team, on_delete=models.CASCADE, related_name="winner", null=True)
+	# 	loser = models.ForeignKey(away_team, on_delete=models.CASCADE, related_name="loser", null=True)
 	# elif away_score > home_score:
-	# 	winner = models.ForeignKey(away_team, on_delete=models.CASCADE)
-	# 	loser = models.ForeignKey(home_team, on_delete=models.CASCADE)
+	# 	winner = models.ForeignKey(away_team, on_delete=models.CASCADE, related_name="winner", null=True)
+	# 	loser = models.ForeignKey(home_team, on_delete=models.CASCADE, related_name="loser", null=True)
 	# else:
 	# 	tied_game = True
 
 	def __str__(self):
 		# return 'The %s played the %s in week %s' (self.home_team, self.away_team, self.week)
-		return 'Week: %s, score: %s to %s' (self.week, self.home_score, self.away_score)
+		return 'Week: %s, score: %s to %s' % (self.week, self.home_score, self.away_score)
 
--- nfl_scores/views.py
+++ nfl_scores/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from .models import Team, Game
+
+def week_view(request):
+	teams = Team.objects.all()
+	games = Game.objects.all()
+	# team_output = ', '.join([str(team.name) for team in teams])
+	# games = ', '.join([str(game) for game in games])
+
+	context = {'games': games, 'teams': teams}
+
+	return render(request, 'nfl_scores/week_view.html', context)
+
+	# return HttpResponse("The current teams are: " + team_output + "\n" + games)
