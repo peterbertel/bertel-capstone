026b8c2b1e92c6a046b64474d664495644be72c1
Peter Bertel <pbertel@Peters-MacBook-Pro.local>
2016-11-22 11:06:52 -0600

    Modified url routing to determine the week with the query string

--- nfl_scores/models.py
+++ nfl_scores/models.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
 class Team(models.Model):
 	name = models.CharField(max_length=200, unique=True)
 	wins = models.IntegerField(default=0)
@@ -11,33 +10,14 @@ class Team(models.Model):
 	def __str__(self):
 		return 'The %s have %s wins and %s losses' % (self.name, self.wins, self.losses)
 
-# class Week(models.Model):
-# 	number = models.IntegerField(default=1)
-
-# 	def __str__(self):
-# 		return 'Week %s' % (self.number)
-
 class Game(models.Model):
-	# home and away team references are problematic
 	home_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="home_team", null=True)
 	away_team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="away_team", null=True)
 	week = models.IntegerField(default=1)
 	home_score = models.IntegerField(default=0)
 	away_score = models.IntegerField(default=0)
-	# maybe can do home_score = away_score = models.IntegerField(default=0)
-	# tied_game = models.BooleanField(default=False)
-
-	# if home_score > away_score:
-	# 	winner = models.ForeignKey(home_team, on_delete=models.CASCADE, related_name="winner", null=True)
-	# 	loser = models.ForeignKey(away_team, on_delete=models.CASCADE, related_name="loser", null=True)
-	# elif away_score > home_score:
-	# 	winner = models.ForeignKey(away_team, on_delete=models.CASCADE, related_name="winner", null=True)
-	# 	loser = models.ForeignKey(home_team, on_delete=models.CASCADE, related_name="loser", null=True)
-	# else:
-	# 	tied_game = True
 
 	def __str__(self):
-		# return 'The %s played the %s in week %s' (self.home_team, self.away_team, self.week)
 		return 'Week: %s, The %s scored %s points against the %s, who scored %s points' % (self.week, self.home_team.name, \
 			self.home_score, self.away_team.name, self.away_score)
 
