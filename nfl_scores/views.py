66b4db958f4f1c953333cc769cec3741865f5828
Peter Bertel <pbertel@Peters-MacBook-Pro.local>
2016-12-11 22:04:54 -0600

    Added standings page with navigation to and from this page. Data is populated correctly

--- nfl_scores/views.py
+++ nfl_scores/views.py
@@ -4,12 +4,21 @@ from django.http import HttpResponse
 from .models import Team, Game
 
 import nflgame
+import httplib
+import json
 
 def week_view(request):
 	w = int(request.GET.get('w', 1))
 
 	teams = Team.objects.all()
 	games = Game.objects.filter(week=w)
+
+	if len(games) == 0:
+		# create a bunch of fake games here, getting them from cache or the sportradar api
+		# think about how you want to change the week_view template for the future games
+		# 	need a way to show the records instead of the final score. Also, the games should
+		#	not be clickable
+		return HttpResponse('This week has not happened yet, that sucks')
 
 	context = {'games': games, 'week': w}
 
@@ -30,6 +39,17 @@ def load_games(request):
 	games = nflgame.games(2016, week=w)
 	if len(games) == 0:
 		return HttpResponse('This week has not happened yet, get the data some other way')
+		# conn = httplib.HTTPSConnection("api.sportradar.us")
+
+		# conn.request("GET", "/nfl-ot1/games/2006/reg/schedule.xml?api_key=mk5mjt48drputswsxqct2uac")
+
+		# res = conn.getresponse()
+		# data = res.read()
+		# data = data.decode("utf-8")
+		# data = json.loads(data)
+
+		# here, enter the debugger and see if you can get games for the week,
+		# show the teams records on the right instead of the final score
 
 	for game in games:
 
@@ -78,14 +98,11 @@ def update_records(request):
 	return HttpResponse("Updated the teams's records")
 
 def standings(request):
-	import httplib
-	import json
 	conn = httplib.HTTPSConnection("api.sportradar.us")
 	conn.request("GET", "/nfl-ot1/seasontd/2016/standings.json?api_key=mk5mjt48drputswsxqct2uac")
 	res = conn.getresponse()
 	data = res.read()
 	data = data.decode('utf-8')
-	# import pdb; pdb.set_trace()
 	data = json.loads(data)
 	# x['conferences'][0]['divisions'][0]['name']
 	# AFC East
@@ -99,16 +116,10 @@ def standings(request):
 			for team in division['teams']:
 				return_string = return_string + team['name'] + " " + str(team['wins']) + "-" + str(team['losses']) + "<br/>"
 
-	return HttpResponse("Standings page: <br/>" + return_string)
+	context = {'data': data}
+	return render(request, 'nfl_scores/standings.html', context)
+	# return HttpResponse("Standings page: <br/>" + return_string)
 
-# Using the SportRadar API
-# import http.client
-
-# conn = http.client.HTTPSConnection("api.sportradar.us")
-
-# conn.request("GET", "/nfl-ot1/games/b7aeb58f-7987-4202-bc41-3ad9a5b83fa4/pbp.xml?api_key={mk5mjt48drputswsxqct2uac}")
-
-# res = conn.getresponse()
-# data = res.read()
-
-# print(data.decode("utf-8"))
+# SportRadar API Key
+# api_key=mk5mjt48drputswsxqct2uac
+# consider making this a variable at the top of the file
