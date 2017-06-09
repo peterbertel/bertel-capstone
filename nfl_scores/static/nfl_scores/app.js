abaffc598e22f3562cbb46137b1073f106cab386
Peter Bertel <pbertel@Peters-MacBook-Pro.local>
2017-06-09 13:20:04 -0500

    Changed the app to only use jquery UI autocomplete for the dropdown search bar so the code is the same across all browsers. Deleted a bunch of unnecessary javascript

--- nfl_scores/static/nfl_scores/app.js
+++ nfl_scores/static/nfl_scores/app.js
@@ -27,7 +27,7 @@ $(".game").click( function() {
 				alert('Error, try again');
 			}
 		});
-		} 
+	}
 	else {
 		gameElement.find('input').prop('checked', false);
 		gameElement.find('.away_q1').text("")
@@ -38,67 +38,25 @@ $(".game").click( function() {
 		gameElement.find('.home_q2').text("")
 		gameElement.find('.home_q3').text("")
 		gameElement.find('.home_q4').text("")
-		}
-	});
-
-var dataList = document.getElementById('json-datalist');
-var teamSet = new Set();
-
-// The following code came from https://github.com/thgreasi/datalist-polyfill
-var browserSupportsDatalist = !!('list' in document.createElement('input')) && !!(document.createElement('datalist') && window.HTMLDataListElement);
+	}
+});
 
 // autocomplete code found from https://jqueryui.com/autocomplete/
-if (!browserSupportsDatalist) {
-	teamList = []
-	$("#search-query").attr('autocomplete','on');
-	$.ajax({
-		url: '/get_teams',
-		type: 'get',
-		dataType: 'json',
-		data: {'searchString': ""},
-		success: function(data) {
-			teams = data.teams;
-			for (var i = 0; i < teams.length; i++) {
-				teamList.push(teams[i].name);
-			}
-			$("#search-query").autocomplete({ source: teamList });
-		},
-		failure: function(data) {
-			alert('Error, try again');
+teamList = []
+$("#search-query").attr('autocomplete','on');
+$.ajax({
+	url: '/get_teams',
+	type: 'get',
+	dataType: 'json',
+	data: {'searchString': ""},
+	success: function(data) {
+		teams = data.teams;
+		for (var i = 0; i < teams.length; i++) {
+			teamList.push(teams[i].name);
 		}
-	});
-}
-$("#search-team").on('input',function(e){
-	if (browserSupportsDatalist) {
-		var searchString = $("#search-query").val();
-
-		// Referenced the following link to create the ajax call:
-		//    https://simpleisbetterthancomplex.com/tutorial/2016/08/29/how-to-work-with-ajax-request-with-django.html
-		// Referenced the following link to create the dropdown form:
-		//    http://blog.teamtreehouse.com/creating-autocomplete-dropdowns-datalist-element
-		$.ajax({
-			url: '/get_teams',
-			type: 'get',
-			dataType: 'json',
-			data: {'searchString': searchString},
-			success: function(data) {
-				teams = data.teams;
-				for (var i = 0; i < teams.length; i++) {
-					teamName = teams[i].name;
-					if (!teamSet.has(teamName)) {
-						teamSet.add(teamName);
-						var option = document.createElement('option');
-						option.value = teamName;
-						dataList.appendChild(option);
-					}
-					else {
-						continue;
-					}
-				}
-			},
-			failure: function(data) {
-				alert('Error, try again');
-			}
-		});
+		$("#search-query").autocomplete({ source: teamList });
+	},
+	failure: function(data) {
+		alert('Error, try again');
 	}
 });
